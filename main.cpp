484fe86b43d6afe03cc63a94fdbb044c89d8f922
nathan-tripp <92132223+nathan-tripp@users.noreply.github.com>
2021-10-07 20:56:39 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+//#include "dna_map.hpp"
+//#include "bins.hpp"
+#include "dna.hpp"
+
+signed int
+main(void)
+{
+	dna_t dna;
+
+/*	bins_t& bins(bins_t::instance());
+
+	dna_map_t dm;
+	std::vector< uint8_t > vec;
+
+	vec.push_back(0x41);
+	vec.push_back(0x42);
+	vec.push_back(0x43);
+	vec.push_back(0x44);
+	vec.push_back(0x45);
+	vec.push_back(0x46);
+	dm.set(vec);
+	std::cout << "dm: " << dm.to_string() << std::endl;
+	dm.get();*/
+	return 0;
+}
